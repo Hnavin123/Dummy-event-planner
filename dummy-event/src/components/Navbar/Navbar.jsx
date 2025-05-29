3693d129eefb75cde461f6f9f2e5e2fd23fbec7f
Harsh Navin <hnavin2132002@gmail.com>
2025-05-30 01:03:54 +0530

    searchbar added to navbar

--- dummy-event/src/components/Navbar/Navbar.jsx
+++ dummy-event/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <input
       type="text"
       placeholder="Search..."
-      className=" px-3 py-1 border border-gray-300 rounded-md"
+      className="mt-10 px-3 py-1 border border-gray-300 rounded-md"
     />
 
       </div>

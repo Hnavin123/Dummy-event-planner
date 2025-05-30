6885ea0d0e5de54a563f9e8b7ad606e79cb1e00d
Harsh Navin <hnavin2132002@gmail.com>
2025-05-30 08:01:39 +0530

    navbar updated

--- dummy-event/src/components/Navbar/Navbar.jsx
+++ dummy-event/src/components/Navbar/Navbar.jsx
@@ -3,26 +3,24 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className='w-full bg-gradient-to-b from-[#b174da] to-[#ffffff]'>
-      <div className="flex items-center gap-12 mb-2">
-        <span>India's Favourite Event Planner Platform</span> 
-        <input
-      type="text"
-      placeholder="Search..."
-      className="mt-10 px-3 py-1 border border-gray-300 rounded-md"
-    />
-
-      </div>
-      <div className='navbar-logo'>
-        <Link to='/'>Event planner</Link>
+    
+    <div className="flex items-center gap-8">
+      <div className='navbar-logo text-xl font-bold text-purple-800'>
+        <Link to='/'>Event Planner</Link>
       </div>
 
-      <ul>
+      <ul className="flex gap-6 text-gray-700 font-medium">
         <li><Link to='/venues'>Venue</Link></li>
         <li><Link to='/vendors'>Vendor</Link></li>
         <li><Link to='/photos'>Photo</Link></li>
         <li><Link to='/e-invite'>E-Invite</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
       </ul>
-    </nav>
+    </div>
+
+    
+  </nav>
   );
 };
 

ccba648c14c29df54e3edd4450869d7ff82ad98e
Harsh Navin <hnavin2132002@gmail.com>
2025-06-02 01:38:35 +0530

    working on home page

--- dummy-event/src/components/EventCards/EventCards.jsx
+++ dummy-event/src/components/EventCards/EventCards.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const EventCards = ({image, title}) => {
     return (
-        <div className="bg-white shadow-md rounded-lg overflow-hidden w-80">
-            <img src={image} alt={title} className='w-full h-48 object-cover' />
+        <div className="bg-white shadow-md rounded-lg overflow-hidden w-80 ">
+            <img src={image} alt={title} className='w-22 object-cover' />
             <div className='p-4'>
                 <h3 className="text-xl font-semibold mb-2">{title}</h3>
             </div>

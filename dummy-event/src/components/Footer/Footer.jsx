39f4680c485b04d648299011993e337bf41f7117
Harsh Navin <hnavin2132002@gmail.com>
2025-06-17 09:57:23 +0530

    footer section updated

--- dummy-event/src/components/Footer/Footer.jsx
+++ dummy-event/src/components/Footer/Footer.jsx
@@ -3,25 +3,18 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <>
-      <section className="footer-background">
-        <div>
-          <div className="footer-logo">
-            <img
-              src="https://jaiswalevents.in/wp-content/uploads/2025/02/jaiswal-event-logo-1.png"
-              alt="footer logo"
-            ></img>
-          </div>
-          <p>
-            Ready to make your event extraordinary? Partner with Jaiswal Events
-            today!
-          </p>
-        </div>
-        <div>
-          <p>Copyright 2024 © All Right Reserved Design by Antler’s Solution</p>
-        </div>
-      </section>
-    </>
+    <section className="footer-background">
+      <div className="footer-logo">
+        <img
+          src="https://jaiswalevents.in/wp-content/uploads/2025/02/jaiswal-event-logo-1.png"
+          alt="footer logo"
+        />
+        <p>Ready to make your event extraordinary? Partner with Jaiswal Events today!</p>
+        <p className="footer-copy">
+          Copyright 2024 © All Right Reserved Design by Instaily
+        </p>
+      </div>
+    </section>
   );
 };
 

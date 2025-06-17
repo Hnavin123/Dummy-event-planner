afefcea60dcc72f490158ea982696356f58f19af
Your name <you@example.com>
2025-06-17 15:16:03 +0530

    navbar ui updated

--- dummy-event/src/components/Navbar/Navbar.jsx
+++ dummy-event/src/components/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
 
   return (
     <>
-      {/* <header>
-        <div className="container header-flex header">
+      <header>
+        <div className="container header-flex">
           <div className="company-logo">
             <img
               src="https://jaiswalevents.in/wp-content/uploads/2025/02/jaiswal-event-logo-1.png"
@@ -17,59 +17,37 @@ const Navbar = () => {
             />
           </div>
 
-          <button
-            className="hamburger"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
+          <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             ☰
           </button>
 
-          <div className={`main-menu ${menuOpen ? 'show' : ''}`}>
+          <div className={`main-menu ${menuOpen ? "show" : ""}`}>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/venues">Venue</a></li>
-              <li><a href="/vendors">Vendor</a></li>
-              <li><a href="/photos">Photos</a></li>
-              <li><a href="/e-invite">E-Invite</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/venues">Venue</a>
+              </li>
+              <li>
+                <a href="/vendors">Vendor</a>
+              </li>
+              <li>
+                <a href="/photos">Photos</a>
+              </li>
+              <li>
+                <a href="/e-invite">E-Invite</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
             </ul>
           </div>
         </div>
-      </header> */}
-
-
-
-      <header className="header">
-  <div className="container header-flex">
-    <div className="company-logo">
-      <img
-        src="https://jaiswalevents.in/wp-content/uploads/2025/02/jaiswal-event-logo-1.png"
-        alt="Company Logo"
-      />
-    </div>
-
-    <button
-      className="hamburger"
-      onClick={() => setMenuOpen(!menuOpen)}
-    >
-      ☰
-    </button>
-  </div>
-
-  <div className={`main-menu ${menuOpen ? 'show' : ''}`}>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/venues">Venue</a></li>
-      <li><a href="/vendors">Vendor</a></li>
-      <li><a href="/photos">Photos</a></li>
-      <li><a href="/e-invite">E-Invite</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/contact">Contact</a></li>
-    </ul>
-  </div>
-</header>
-
+      </header>
     </>
   );
 };

c04aadb4c7f32c04f8eb96a45be5e5c0310eb5f1
Harsh Navin <hnavin2132002@gmail.com>
2025-06-17 11:57:58 +0530

    event card creted

--- dummy-event/src/components/Navbar/Navbar.jsx
+++ dummy-event/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <>
-      <header>
+      {/* <header>
         <div className="container header-flex header">
           <div className="company-logo">
             <img
@@ -36,7 +36,40 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </header>
+      </header> */}
+
+
+
+      <header className="header">
+  <div className="container header-flex">
+    <div className="company-logo">
+      <img
+        src="https://jaiswalevents.in/wp-content/uploads/2025/02/jaiswal-event-logo-1.png"
+        alt="Company Logo"
+      />
+    </div>
+
+    <button
+      className="hamburger"
+      onClick={() => setMenuOpen(!menuOpen)}
+    >
+      ☰
+    </button>
+  </div>
+
+  <div className={`main-menu ${menuOpen ? 'show' : ''}`}>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/venues">Venue</a></li>
+      <li><a href="/vendors">Vendor</a></li>
+      <li><a href="/photos">Photos</a></li>
+      <li><a href="/e-invite">E-Invite</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </div>
+</header>
+
     </>
   );
 };
